6527441c4b81a13f664d2511bb3e2e7bbd0e8802
NazrinGuliyevaLV <Nazrin.Guliyeva@lv-logistics.com>
2025-07-18 14:38:51 +0400

    added dynamic resize

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 
 
 
-const app = express(); 
+const app = express();
 
 
 app.use(express.json({ limit: '50mb' }));
@@ -15,43 +15,42 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json());
 app.post('/add-image', async (req, res) => {
   try {
-    const { excelFileContent, imageContent, fileName } = req.body;
+    const {x,y,h,w, excelFileContent, imageContent, fileName } = req.body;
 
     if (!excelFileContent || !imageContent || !fileName) {
       return res.status(400).json({ error: 'Missing fields in request' });
     }
- 
+
     const tempExcelPath = path.join(__dirname, 'temp-input.xlsx');
     fs.writeFileSync(tempExcelPath, Buffer.from(excelFileContent, 'base64'));
- 
+
     const workbook = new Excel.Workbook();
     await workbook.xlsx.readFile(tempExcelPath);
 
     const worksheet = workbook.getWorksheet('form') || workbook.addWorksheet('form');
- 
+
     const ext = path.extname(fileName).slice(1) || 'jpeg';
     const imageBuffer = Buffer.from(imageContent, 'base64');
 
-   
-
     const imageId = workbook.addImage({
-  buffer: imageBuffer,
-  extension: ext
-});
- 
-worksheet.getRow(9).height = 80;
-worksheet.getColumn(2).width = 25;
- 
-worksheet.addImage(imageId, {
-  tl: { col: 1, row: 46 },  
-  ext: { width: 150, height: 100 }
-});
- 
- 
+      buffer: imageBuffer,
+      extension: ext
+    });
+
+    worksheet.addImage(imageId, {
+      tl: { col: x, row: y },
+      ext: { width: w, height: h }
+    });
+
     const outputPath = path.join(__dirname, `output_${Date.now()}.xlsx`);
+
     await workbook.xlsx.writeFile(outputPath);
 
-    res.json({ success: true, savedFile: path.basename(outputPath) });
+    const fileBuffer = fs.readFileSync(outputPath);
+    const base64Excel = fileBuffer.toString('base64');
+
+    fs.unlinkSync(outputPath);
+    res.send(base64Excel)
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: err.message });
