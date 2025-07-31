0889c5aa483c96e019ff7a26566084e4d31183f5
NazrinGuliyevaLV <Nazrin.Guliyeva@lv-logistics.com>
2025-07-31 10:23:16 +0400

    update index.js

--- index.js
+++ index.js
@@ -1,60 +1,124 @@
-const express = require('express');
+const { app } = require('@azure/functions');
+
 const Excel = require('exceljs');
+
 const fs = require('fs');
+
+const os = require('os');
+
 const path = require('path');
-const bodyParser = require('body-parser')
 
+const tempDir = os.tmpdir();
+ 
+const randomName = (len) => {
 
+    let name = '';
 
-const app = express();
+    for (let i = 0; i < len; i++) {
 
+        name += String(parseInt(Math.random() * 10));
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+    };
 
-app.use(bodyParser.json());
-app.post('/add-image', async (req, res) => {
-  try {
-    const {x,y,h,w, excelFileContent, imageContent, fileName } = req.body;
+    return name;
 
-    if (!excelFileContent || !imageContent || !fileName) {
-      return res.status(400).json({ error: 'Missing fields in request' });
+};
+ 
+app.http('InsertImageXlsx', {
+
+    methods: ['POST'],
+
+    authLevel: 'anonymous',
+
+    handler: async (req, context) => {
+
+        context.log(`Http function processed request for url "${req.url}"`);
+ 
+        try {
+
+            const { x, y, h, w, sheet, excelFileContent, imageContent } = await req.json();
+ 
+            if (!excelFileContent || !imageContent) {
+
+                return {
+
+                    status: 400,
+
+                    jsonBody: { error: 'Missing fields in request' }
+
+                };
+
+            }
+ 
+            const inputPath = path.join(tempDir, `input-${randomName(10)}.xlsx`);
+
+            fs.writeFileSync(inputPath, Buffer.from(excelFileContent, 'base64'));
+ 
+            const workbook = new Excel.Workbook();
+
+            await workbook.xlsx.readFile(inputPath);
+
+            const worksheet = workbook.getWorksheet(sheet);
+ 
+            const imageBuffer = Buffer.from(imageContent, 'base64');
+
+            const imageId = workbook.addImage({
+
+                buffer: imageBuffer,
+
+                extension: 'png'
+
+            });
+ 
+            worksheet.addImage(imageId, {
+
+                tl: { col: x, row: y },
+
+                ext: { width: w, height: h }
+
+            });
+ 
+            const outputPath = path.join(tempDir, `output-${randomName(10)}.xlsx`);
+
+            await workbook.xlsx.writeFile(outputPath);
+ 
+            const fileBuffer = fs.readFileSync(outputPath);
+
+            const base64Excel = fileBuffer.toString('base64');
+ 
+            fs.unlinkSync(inputPath);
+
+            fs.unlinkSync(outputPath);
+ 
+            return {
+
+                status: 201,
+
+                body: base64Excel
+
+            };
+ 
+        } catch (err) {
+
+            context.log('err message::', err.message);
+
+            context.log('err stack:', err.stack);
+
+            return {
+
+                status: 500,
+
+                jsonBody: {
+
+                    error: 'Internal server error'
+
+                }
+
+            };
+
+        }
+
     }
 
-    const tempExcelPath = path.join(__dirname, 'temp-input.xlsx');
-    fs.writeFileSync(tempExcelPath, Buffer.from(excelFileContent, 'base64'));
-
-    const workbook = new Excel.Workbook();
-    await workbook.xlsx.readFile(tempExcelPath);
-
-    const worksheet = workbook.getWorksheet('Sheet1') || workbook.addWorksheet('Sheet1');
-
-    const ext = path.extname(fileName).slice(1) || 'png';
-    const imageBuffer = Buffer.from(imageContent, 'base64');
-
-    const imageId = workbook.addImage({
-      buffer: imageBuffer,
-      extension: ext
-    });
-
-    worksheet.addImage(imageId, {
-      tl: { col: x, row: y },
-      ext: { width: w, height: h }
-    });
-
-    const outputPath = path.join(__dirname, `output_${Date.now()}.xlsx`);
-
-    await workbook.xlsx.writeFile(outputPath);
-
-    const fileBuffer = fs.readFileSync(outputPath);
-    const base64Excel = fileBuffer.toString('base64');
-
-    fs.unlinkSync(outputPath);
-    res.send(base64Excel)
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
 });
-
-app.listen(3000, () => console.log('API listening on http://localhost:3000'));
+ 
